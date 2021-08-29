cfe48a066d7e863aa6c4d531b488eaffd4ce848c
LeoGao <zgao11@huskers.unl.edu>
2021-08-29 22:42:44 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Leo Gao
+ * Date: 2020/8/30
+ * Lab 1 hello world
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello world!\n");
+
+    return 0;
+}
