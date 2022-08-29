e0461f8165439f981c3f2b2e1f35b7321dd47726
Leo Gao <zgao11@huskers.unl.edu>
2022-08-29 21:57:20 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -2,15 +2,16 @@
   Programmer's Name:Leo Gao
   NUID: 46878390
   Date: 8/29/2022
-  Description of the program: My name!
+  Description of the program: My name
 */
 #include<stdio.h>
 #include<math.h>
 
     int main(void) {
-    int daystotal=0;
 
     printf("Leo Gao!\n");
+
+    printf("Computer Science!\n");
 
     return 0;
     }
